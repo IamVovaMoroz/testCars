364b63c1cf72ce2c993bf82f6994937debc79aa6
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-18 20:52:13 +0200

    pagination

--- src/components/CarContainer.js
+++ src/components/CarContainer.js
@@ -1,13 +1,25 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import CarCard from './CarCard'; // Импортируем компонент CarCard
 import './CarContainer.css'; // Импортируем файл стилей
 
 const CarContainer = ({ cars }) => {
+    const [carsToShow, setCarsToShow] = useState(12); // Изначально показываем 12 машин
+
+    const loadMoreCars = () => {
+        setCarsToShow(carsToShow + 12); // Увеличиваем количество машин на 12
+      };
+
   return (
     <div className='car-container'>
-      {cars.map((car, index) => (
+      {cars.slice(0, carsToShow).map((car, index) => (
         <CarCard key={index} car={car} />
       ))}
+      {carsToShow < cars.length && (
+        <button className="load-more-button" onClick={loadMoreCars}>
+          Load more
+        </button>
+      )}
     </div>
   );
 };
