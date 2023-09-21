a05016fec321c2c9ea16ca7bbd7c69e381b5696e
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-09-21 16:37:01 +0200

    styled

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import myImage from './../salon.jpg'; // Импорт изображения
-
 
 const Home = () => {
   const containerStyle = {
@@ -21,16 +19,22 @@ const Home = () => {
     textAlign: 'center',
   };
 
+  const imageStyle = {
+    width: '100%', // Установите 100% для полного заполнения родительского контейнера
+    maxWidth: '800px', // Максимальная ширина изображения
+    height: 'auto', // Высота автоматически масштабируется, чтобы сохранить пропорции
+  };
+
   return (
     <div style={containerStyle}>
       <h1 style={headingStyle}>Welcome to our car catalog</h1>
       <p style={descriptionStyle}>
-      Here you can find a wide selection of cars of different brands and classes.
-         Whether you are looking for a car for travel or business
-         meetings, we have the right option for you. Use filters to
-         customize your search and find the perfect car for your needs.
+        Here you can find a wide selection of cars of different brands and classes.
+        Whether you are looking for a car for travel or business
+        meetings, we have the right option for you. Use filters to
+        customize your search and find the perfect car for your needs.
       </p>
-      <img src={myImage} alt="Car Catalog Cover" />
+      <img src={myImage} alt="Car Catalog Cover" style={imageStyle} />
     </div>
   );
 };
